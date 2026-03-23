a7986711ae519577d1432dd546ec069497719e6a
lichunqing <cybzq37@gmail.com>
2026-03-23 21:08:27 +0800

    ch2

--- ch2/helloSLAM.cpp
+++ ch2/helloSLAM.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
+#include "libHelloSLAM.h"
 using namespace std;
 
 int main(int argc, char** argv)
 {
-    cout << "Hello SLAM!" << endl;
+    printHello();
     return 0;
 }
---
+++ ch2/libHelloSLAM.h
@@ -0,0 +1,6 @@
+#ifndef LIB_HELLOSLAM_H
+#define LIB_HELLOSLAM_H
+
+void printHello();
+
+#endif // LIB_HELLOSLAM_H
