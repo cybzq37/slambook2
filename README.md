e2c37f08fd3d1add7c0dcbadff78d3b4f63be6c7
cybzq37 <cybzq37@gmail.com>
2026-03-24 14:10:11 +0800

    draw trajectory

--- README.md
+++ README.md
@@ -1,5 +1,36 @@
 # slambook2
 
+安装环境
+```bash
+apt install -y build-essential cmake
+apt install -y python3-dev python3-numpy python3-pip
+apt install -y libeigen3-dev
+apt install -y libopencv-dev
+```
+
+安装 opengl
+```bash
+# ubuntu
+# libgl1-mesa-dev        OpenGL 核心开发库（Mesa 实现），提供 libGL.so，用于基础图形渲染
+# libglu1-mesa-dev       GLU 工具库，在 OpenGL 之上提供高层功能（如投影、坐标变换等）
+# libglfw3-dev           GLFW 窗口与输入库，用于创建窗口、处理键盘鼠标、管理 OpenGL 上下文
+# libglew-dev            GLEW 扩展加载库，用于访问现代 OpenGL 的扩展函数（解决函数指针加载问题）
+# libglm-dev             GLM 数学库，提供向量/矩阵/变换运算（类似 Eigen，但专为图形设计）
+# libglvnd-dev           GLVND 调度层，统一管理不同厂商（NVIDIA/Mesa）的 OpenGL 实现
+# libegl1-mesa-dev       EGL 开发库，用于创建 OpenGL 上下文（支持无窗口/嵌入式/Wayland 环境）
+# libepoxy-dev           Epoxy 扩展加载库（常用于 GTK/Pango），用于自动加载 OpenGL 函数（替代 GLEW）
+
+sudo apt install -y \
+libgl1-mesa-dev \
+libglu1-mesa-dev \
+libglfw3-dev \
+libglew-dev \
+libglm-dev \
+libglvnd-dev \
+libegl1-mesa-dev \
+libepoxy-dev
+```
+
 添加子模块
 ```bash
 git submodule add https://github.com/stevenlovegrove/Pangolin 3rdparty/Pangolin
@@ -8,4 +39,19 @@ git submodule add https://github.com/ceres-solver/ceres-solver 3rdparty/ceres-so
 git submodule add https://github.com/RainerKuemmerle/g2o 3rdparty/g2o
 git submodule add https://github.com/rmsalinas/DBoW3 3rdparty/DBoW3
 git submodule add https://github.com/google/googletest.git 3rdparty/googletest
+```
+
+查看子模块状态
+```bash
+git submodule status
+```
+
+安装子模块
+```bash
+# 安装 Pangolin，参考3rdparty/Pangolin中的readme（先安装依赖）
+cd 3rdparty/Pangolin
+mkdir build && cd build
+cmake ..
+make -j
+sudo make install
 ```
--- ch3/examples/plotTrajectory.cpp
+++ ch3/examples/plotTrajectory.cpp
@@ -3,10 +3,93 @@
 #include <unistd.h>
 
 using namespace std;
-using namepsace Eigen;
+using namespace Eigen;
+
+string trajectory_file = "../trajectory.txt";
+
+void DrawTrajectory(vector<Isometry3d, aligned_allocator<Isometry3d>> trajectory);
 
 int main(int argc, char** argv) {
+    vector<Isometry3d, aligned_allocator<Isometry3d>> poses;
+    ifstream fin(trajectory_file);
+    if(!fin) {
+        cerr << "cannot find trajectory file at " << trajectory_file << endl;
+        return 1;
+    }
 
+    while(!fin.eof()) {
+        // tx ty tz 相机位置，qx qy qz qw 相机姿态四元数
+        double time, tx,ty,tz,qx,qy,qz,qw;
+        fin >> time >> tx >> ty >> tz >> qx >> qy >> qz >> qw;
+        Isometry3d Twr(Quaterniond(qw, qx, qy, qz));
+        Twr.pretranslate(Vector3d(tx, ty, tz)); // 将平移部分添加到变换矩阵中
+        poses.push_back(Twr);
+    }
+
+    cout << "read total " << poses.size() << " pose entries" << endl;
+
+    DrawTrajectory(poses);
 
     return 0;
+}
+
+void DrawTrajectory(vector<Isometry3d, aligned_allocator<Isometry3d>> trajectory) {
+    if(trajectory.empty()) {
+        cerr << "trajectory is empty!" << endl;
+        return;
+    }
+
+    // create pangolin window and plot the trajectory
+    pangolin::CreateWindowAndBind("Trajectory Viewer", 1024, 768);
+    glEnable(GL_DEPTH_TEST);
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+    pangolin::OpenGlRenderState s_cam(
+            pangolin::ProjectionMatrix(1024, 768, 500, 500, 512, 389, 0.1, 1000),
+            pangolin::ModelViewLookAt(0, -0.1, -1.8, 0, 0, 0, 0.0, -1.0, 0.0)
+    );
+
+    pangolin::Handler3D handler(s_cam);
+    pangolin::View& d_cam = pangolin::CreateDisplay()
+            .SetBounds(0.0, 1.0, pangolin::Attach::Pix(175), 1.0, -1024.0f/768.0f)
+            .SetHandler(&handler);
+
+    while(pangolin::ShouldQuit() == false) {
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        d_cam.Activate(s_cam);
+        glClearColor(1.0f,1.0f,1.0f,1.0f);
+
+        // draw trajectory
+        glLineWidth(2);
+        for(size_t i=0; i<trajectory.size(); i++) {
+            Vector3d Ow = trajectory[i].translation(); // 相机中心
+            Vector3d Xw = trajectory[i] * (0.1 * Vector3d(1,0,0)); // 相机坐标系的x轴
+            Vector3d Yw = trajectory[i] * (0.1 * Vector3d(0,1,0)); // 相机坐标系的y轴
+            Vector3d Zw = trajectory[i] * (0.1 * Vector3d(0,0,1)); // 相机坐标系的z轴
+            glBegin(GL_LINES);
+            glColor3f(1.0f,0.0f,0.0f); // 红色表示x轴
+            glVertex3d(Ow[0], Ow[1], Ow[2]);
+            glVertex3d(Xw[0], Xw[1], Xw[2]);
+            glColor3f(0.0f,1.0f,0.0f); // 绿色表示y轴
+            glVertex3d(Ow[0], Ow[1], Ow[2]);
+            glVertex3d(Yw[0], Yw[1], Yw[2]);
+            glColor3f(0.0f,0.0f,1.0f); // 蓝色表示z轴
+            glVertex3d(Ow[0], Ow[1], Ow[2]);
+            glVertex3d(Zw[0], Zw[1], Zw[2]);
+            glEnd();
+        }
+
+        glColor3f(0.0f, 0.0f, 0.0f); // 黑色表示轨迹线
+        glBegin(GL_LINE_STRIP);
+        for (size_t i = 0; i < trajectory.size(); i++) {
+            auto p = trajectory[i].translation();
+            glVertex3d(p[0], p[1], p[2]);
+        }
+        glEnd();
+
+        pangolin::FinishFrame();
+        usleep(50000);   // sleep 50 ms
+    }
 }
